d523d5c63653b7b2a201d991bc0aaae42d8d8a4b
svarchevskyiyan@gmail.com <84012931+welchowski@users.noreply.github.com>
2021-08-07 14:12:48 +0300

    dfg

---
+++ test4.py
@@ -0,0 +1,40 @@
+import os
+import time 
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+    from selenium import webdriver
+    browser = webdriver.Chrome()
+    
+    browser.get(link)
+    
+    time.sleep(2)
+    button=browser.find_element_by_xpath("//button[@type='submit']").click()
+    
+    time.sleep(2)
+    confirm = browser.switch_to.alert
+    
+    confirm.accept()
+    value=browser.find_element_by_id("input_value").text
+    print(value)
+    ivalue=calc(value)
+    print(ivalue)
+    input=browser.find_element_by_id("answer").send_keys(str(ivalue))
+    button=browser.find_element_by_xpath("//button[@type='submit']").click()
+
+
+
+   
+
+
+finally:
+    time.sleep(5)
+    browser.quit()
+
